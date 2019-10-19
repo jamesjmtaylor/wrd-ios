c4d09ad67dd96f4d6d6ac63be415a93df356db2f
jamesjmtaylor <james.jm.taylor@gmail.com>
2019-10-19 08:52:39 -0700

    Got tab bar working with subviews

---
+++ wrdcalculator/HeatView.swift
@@ -0,0 +1,21 @@
+//
+//  HeatView.swift
+//  wrdcalculator
+//
+//  Created by James Taylor on 10/19/19.
+//  Copyright © 2019 James Taylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct HeatView: View {
+    var body: some View {
+        Text("Heat View")
+    }
+}
+
+struct HeatView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeatView()
+    }
+}
---
+++ wrdcalculator/KeView.swift
@@ -0,0 +1,21 @@
+//
+//  KeView.swift
+//  wrdcalculator
+//
+//  Created by James Taylor on 10/19/19.
+//  Copyright © 2019 James Taylor. All rights reserved.
+//
+
+import SwiftUI
+
+struct KeView: View {
+    var body: some View {
+        Text("KE View")
+    }
+}
+
+struct KeView_Previews: PreviewProvider {
+    static var previews: some View {
+        KeView()
+    }
+}
